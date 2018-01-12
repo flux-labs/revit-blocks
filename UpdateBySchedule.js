89b96b1740caed777105e4a1d6b79c8502765ab1
thomastrinelle <thomas@flux.io>
2018-01-11 16:23:19 -0800

    Implemented a brute force method to manually update parameters when 'updateParameters' fail

--- UpdateBySchedule.js
+++ UpdateBySchedule.js
@@ -41,14 +41,13 @@ function newParamMap(){return new Object};
  *
  */
 function run(revitElements, scheduleUpdate) {
-	var revitElements;
 	//console.log(_.nth(scheduleUpdate,0));
 	/*
-//	console.log(topRow);
+	//	console.log(topRow);
 	//console.log(revitElements.length);
 	var newRow = function(){var Row = []; return Row};
 	var schedule = [];
-//	schedule.push(parameterList);
+	//	schedule.push(parameterList);
 	for (var i=0;i<revitElements.length;i++){
 		//console.log("ok");
 		var thisRow = newRow();
@@ -61,45 +60,76 @@ function run(revitElements, scheduleUpdate) {
 		
 	}
 	*/
-//console.log(scheduleUpdate.length);
-var setParams = scheduleUpdate.shift();
-
-
-// UPDATE REVIT ELEMENTS BASED ON INCOMING UPDATED SCHEDULE
-var ParameterMap;
-
-for(var z = 0; z<scheduleUpdate.length;z++){
-	ParameterMap = newParamMap();
+	//console.log(scheduleUpdate.length);
+	var setParams = scheduleUpdate.shift();
 	
-	for(var g = 0;g<setParams.length;g++){
-		core.Set(ParameterMap, setParams[g], scheduleUpdate[z][g]);
+	// UPDATE REVIT ELEMENTS BASED ON INCOMING UPDATED SCHEDULE
+	var ParameterMap;
+	
+	for(var z = 0; z<scheduleUpdate.length;z++){
+		ParameterMap = newParamMap();
+		var path = [];
+		var oldValues = [];
+		var newValues = [];
+		var curValues = [];
+		var attributes = Object.keys(revitElements[z]);
+		
+		for (var i in setParams){
+			core.Set(ParameterMap, setParams[i], scheduleUpdate[z][i]);
+			
+			// Finding the path of the attribute
+			for (var j in attributes){
+				var paramList = Object.keys(revitElements[z][attributes[j]])
+				if (paramList.includes(setParams[i])){
+					path.push(attributes[j])
+					
+					// Listing out the old and desired new values
+					oldValues.push(revitElements[z][attributes[j]][setParams[i]])
+					newValues.push(ParameterMap[setParams[i]])
+					break
+				}
+			}
+		}
+		
+		if (ParameterMap.fluxId != undefined)
+		{
+			//if schedule contains fluxId's, update according to fluxId
+			//console.log(revitElements)
+			matchId("fluxId",revitElements,ParameterMap);
+		}
+		else if(ParameterMap.UniqueId != undefined)
+		{
+			//if schedule contains UniqueId, update according to fluxId
+			//console.log(revitElements)
+			matchId("UniqueId",revitElements,ParameterMap);
+		}
+		else if(ParameterMap.ElementId != undefined)
+		{
+			//if schedule contains Revit elementId's, update according to elementId
+			matchId("ElementId",revitElements,ParameterMap);
+		}
+		else
+		{
+			// OTHERWISE ASSUME ELEMENTS IN SAME ORDER POST EXCEL
+			revit.updateParameters(revitElements[z], ParameterMap);
+		}
+		
+		// Back-up brute force method to manually update the parameters value
+		for (var i in setParams){
+			curValues.push(revitElements[z][path[i]][setParams[i]]);
+		}
+		if (JSON.stringify(curValues) != JSON.stringify(newValues)){
+			// Attempt to hard change them
+			for (var i in setParams){
+				if (path[i]){
+					revitElements[z][path[i]][setParams[i]] = ParameterMap[setParams[i]]
+				}
+			}
+		}
 	}
-	if (ParameterMap.fluxId != undefined)
-	{
-		//if schedule contains fluxId's, update according to fluxId
-		//console.log(revitElements)
-		matchId("fluxId",revitElements,ParameterMap);
-	}
-	else if(ParameterMap.UniqueId != undefined)
-	{
-		//if schedule contains UniqueId, update according to fluxId
-		//console.log(revitElements)
-		matchId("UniqueId",revitElements,ParameterMap);
-	}
-	else if(ParameterMap.ElementId != undefined)
-	{
-		//if schedule contains Revit elementId's, update according to elementId
-		matchId("ElementId",revitElements,ParameterMap);
-	}
-	else
-	{
-		// OTHERWISE ASSUME ELEMENTS IN SAME ORDER POST EXCEL
-		revit.updateParameters(revitElements[z], ParameterMap);
-	}
-}
 
 
-return {revitUpdate:revitElements} 
+	return {revitUpdate:revitElements} 
 	
 }
 
